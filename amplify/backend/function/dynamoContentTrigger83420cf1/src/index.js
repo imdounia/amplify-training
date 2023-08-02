1f1483d997d01b867d2acae82703d31a2ca5cf83
imdounia <douniabakhkhouch2000@gmail.com>
2023-08-02 16:54:22 +0200

    Fix lambda trigger and send comment to mail

--- amplify/backend/function/dynamoContentTrigger83420cf1/src/index.js
+++ amplify/backend/function/dynamoContentTrigger83420cf1/src/index.js
@@ -1,9 +1,53 @@
-exports.handler = function (event, context) {
+const AWS = require('aws-sdk');
+AWS.config.update({ region: 'eu-west-3' });
+const ses = new AWS.SES();
+
+/**
+* @type {import('@types/aws-lambda').DynamoDBStreamHandler}
+*/
+
+// async function verifyEmailAddress(emailAddress) {
+//   try {
+//     const params = {
+//       EmailAddress: emailAddress,
+//     };
+
+//     await ses.verifyEmailAddress(params).promise();
+//     console.log(`Successfully verified email address: ${emailAddress}`);
+//   } catch (error) {
+//     console.error(`Error verifying email address ! ${emailAddress}:`, error);
+//   }
+// }
+
+exports.handler = async (event, context) => {
   console.log(JSON.stringify(event, null, 2));
-  event.Records.forEach(record => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
-  });
-  context.done(null, 'Successfully processed DynamoDB record');
+  try {
+    for (const record of event.Records) {
+      if (record.eventName === "INSERT") {
+        const destination = "douniabakhkhouch2000@gmail.com";
+        const messageContent = record.dynamodb.NewImage.comment.S;
+        // Create the email parameters
+        const params = {
+          Destination: {
+            ToAddresses: [destination],
+          },
+          Message: {
+            Body: {
+              Text: { Data: messageContent },
+            },
+            Subject: { Data: "Sent from my platform" },
+          },
+          Source: "douniabakhkhouch2000@gmail.com",
+
+        };
+        // Send the email using SES
+        await ses.sendEmail(params).promise();
+        console.log(`Email sent to ${destination}`);
+      }
+    }
+    context.done(null, 'Successfully processed DynamoDB record');
+  } catch (error) {
+    console.error('Error sending email:', error);
+    context.done(error, 'Error processing DynamoDB record');
+  }
 };
